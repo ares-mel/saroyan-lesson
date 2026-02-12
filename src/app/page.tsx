fdb9766dea2541c4bab75172000b997ca8d12578
Ares Meliksetyan <aresmeliksetyan1@gmail.com>
2026-02-12 10:41:52 +0400

    Update src/app/page.tsx to include all components with VocabularyDeck section between QuizSection and WritingTask

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
+import React from 'react';
+import Hero from './components/Hero';
+import ContextSection from './components/ContextSection';
+import CinemaSection from './components/CinemaSection';
+import QuizSection from './components/QuizSection';
 import VocabularyDeck from './components/VocabularyDeck';
+import WritingTask from './components/WritingTask';
 
-function Page() {
+const App = () => {
   return (
     <div>
+      <Hero />
+      <ContextSection />
+      <CinemaSection />
       <QuizSection />
       <VocabularyDeck />
       <WritingTask />
     </div>
   );
-}
+};
 
-export default Page;
+export default App;
